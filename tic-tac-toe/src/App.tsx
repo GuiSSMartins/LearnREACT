aee421e0a1ab6fc3553ae0d5caa80aed921728d8
Guilherme Martins <guissmartins2013@gmail.com>
2023-07-04 13:12:59 +0100

    Avanços Jogo do Galo + UI

--- tic-tac-toe/src/App.tsx
+++ tic-tac-toe/src/App.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
-function Square({ value }: {value: string}) {
+function Square() {
+  const [value, setValue] = useState<string | null>(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
   return (
-  <button className="square">{value}</button>
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
   );
 }
 
 export default function Board() {
   return (
     <>
-      <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+<div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
+
